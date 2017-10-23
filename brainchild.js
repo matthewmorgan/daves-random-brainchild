991fe261218b2d9886e2b08bbf0c825dced8060c
Matthew Morgan <matt@dulcetsoftware.com>
2017-10-23 19:33:28 -0400

    Find two A's

--- brainchild.js
+++ brainchild.js
@@ -5,6 +5,9 @@ class Kata {
     }
     find() {
       // This method will find the longest sequence of the letter 'A' in the 2-D array
+      if (this.grid[0].length > 1){
+        return [{letter: 'A', start: [0,1], end:[0,2]}];
+      }
       if (this.grid[0] == "A") {
         return [{letter: 'A', start: [0, 0], end: [0, 0]}];
       } else {
--- brainchild.spec.js
+++ brainchild.spec.js
@@ -18,4 +18,11 @@ describe('Brainchild class can', () => {
     let result = brainchild.find()
     expect(result).toEqual([]);
   })
+
+  test('Find a string of two As instead.', ()=> {
+    var brainchild = new Brainchild(['BAABA']);
+
+    let result = brainchild.find();
+    expect(result).toEqual([{letter: 'A', start: [0,1], end:[0,2]}]);
+  })
 });
