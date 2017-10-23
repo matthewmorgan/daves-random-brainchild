94d0c36cafd34a01e8d1b9c9c60af72409501e6f
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-10-23 19:15:38 -0400

    Simplest test passes

--- brainchild.js
+++ brainchild.js
@@ -1,6 +1,8 @@
 // replace this generic Kata class with your main business class
 class Kata {
-
+    find() {
+      return [{letter: 'A', start: [0,0], end:[0,0]}];
+    }
 }
 
 module.exports = Kata;
--- brainchild.spec.js
+++ brainchild.spec.js
@@ -2,6 +2,13 @@ const Brainchild = require('./brainchild');
 
 describe('Brainchild class can', () => {
   test('be initialized with a grid of letters', ()=> {
-    var brainchild = new Brainchild(['A'])
+    var brainchild = new Brainchild(['A']);
+  })
+
+  test('can find the letter A', ()=> {
+    var brainchild = new Brainchild(['A']);
+
+    let result = brainchild.find();
+    expect(result).toEqual([{letter: 'A', start: [0,0], end:[0,0]}])
   })
 });
