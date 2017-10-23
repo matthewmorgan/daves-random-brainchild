3e75943a1f9b6bab1c762a7e0020db2c44a89254
Matthew Morgan <matt@dulcetsoftware.com>
2017-10-23 19:09:34 -0400

    init

---
+++ brainchild.spec.js
@@ -0,0 +1,7 @@
+const Brainchild = require('./brainchild');
+
+describe('Brainchild class can', () => {
+  test('be initialized with a grid of letters', ()=> {
+    var brainchild = new Brainchild(['A'])
+  })
+});
