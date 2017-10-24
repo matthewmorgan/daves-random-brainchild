033ff9f196a2600b9b52c44739a98bba7096ca17
David Braley <david.braley@gmail.com>
2017-10-24 12:41:05 -0400

    refactor

--- brainchild.js
+++ brainchild.js
@@ -1,13 +1,6 @@
 // replace this generic Kata class with your main business class
 const brainchild = (grid) => {
-  if (grid[0] === 'AA'){
-    return [{letter: 'A', start:[0,0], end: [0,1]}]
-  } else if (grid[0] === 'AAA') {
-    return [{letter: 'A', start: [0, 0], end: [0, 2]}]
-  }else if (grid[0] === 'AAAA') {
-    return [{letter: 'A', start: [0, 0], end: [0, 3]}]
-  }
-  return [{"end": [0, 0], "letter": grid[0], "start": [0, 0]}]
+  return [{"end": [0, grid[0].length - 1], "letter": grid[0].charAt(0), "start": [0, 0]}]
 }
 
 module.exports = brainchild;
