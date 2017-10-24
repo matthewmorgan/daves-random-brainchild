e6643e01691f10ad6c59d211fb2520312029ebaa
David Braley <david.braley@gmail.com>
2017-10-24 11:10:10 -0400

    fix linting errors

--- brainchild.js
+++ brainchild.js
@@ -1,11 +1,11 @@
 // replace this generic Kata class with your main business class
 class Kata {
-    constructor (grid) {
-      this.grid = grid;
-    }
-    find() {
-      return [];
-    }
+  constructor(grid) {
+    this.grid = grid;
+  }
+  find() {
+    return [];
+  }
 }
 
 module.exports = Kata;
--- brainchild.spec.js
+++ brainchild.spec.js
@@ -1,43 +1,42 @@
 const Brainchild = require('./brainchild');
 
 describe('Brainchild class can', () => {
-  test('be initialized with a grid of letters', ()=> {
-    var brainchild = new Brainchild(['A']);
-  })
+  test('be initialized with a grid of letters', () => {
+    new Brainchild(['A']);
+  });
 
-  test('can find the letter A', ()=> {
-    var brainchild = new Brainchild(['A']);
+  test('can find the letter A', () => {
+    const brainchild = new Brainchild(['A']);
 
-    let result = brainchild.find();
-    expect(result).toEqual([{letter: 'A', start: [0,0], end:[0,0]}])
-  })
+    const result = brainchild.find();
+    expect(result).toEqual([{ letter: 'A', start: [0, 0], end: [0, 0] }]);
+  });
 
   // TODO: Write some tests to get to the end of this!
 
-  test('can find the letter two long streams', ()=> {
-    var brainchild = new Brainchild([
+  test('can find the letter two long streams', () => {
+    const brainchild = new Brainchild([
       'AAAAAX',
       'BCDEFB',
       'FGHIJB',
       'JKLMNB',
       'NOPQRB']);
 
-    let result = brainchild.find();
-    expect(result).toEqual([{letter: 'A', start: [0,0], end:[0,4]},
-      {letter:'B', start:[1,5], end:[4,5]}])
-  })
+    const result = brainchild.find();
+    expect(result).toEqual([{ letter: 'A', start: [0, 0], end: [0, 4] },
+      { letter: 'B', start: [1, 5], end: [4, 5] }]);
+  });
 
-test('can find the letter two long streams', ()=> {
-    var brainchild = new Brainchild([
+  test('can find the letter two long streams', () => {
+    const brainchild = new Brainchild([
       'AACDE',
       'BADEF',
       'FAAIJ',
       'JALAN',
       'NAPQA']);
 
-    let result = brainchild.find();
-    expect(result).toEqual([{letter: 'A', start: [0,0], end:[4,4]},
-      {letter:'A', start:[0,1], end:[4,1]}])
-  })
-
+    const result = brainchild.find();
+    expect(result).toEqual([{ letter: 'A', start: [0, 0], end: [4, 4] },
+      { letter: 'A', start: [0, 1], end: [4, 1] }]);
+  });
 });
