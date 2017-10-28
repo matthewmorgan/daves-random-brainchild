fe6b6b42f4ac5ddd015135c24a29c582e05e2dba
Matthew Morgan <matt@dulcetsoftware.com>
2017-10-27 21:04:52 -0400

    Refactor for WWRD

--- brainchild.js
+++ brainchild.js
@@ -1,17 +1,17 @@
-const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LETTERS = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 module.exports = function(grid){
 
   return {
     find: ()=> {
-      const results = [];
       let longest = 0;
-      for (let letter of LETTERS){
-        if (grid[0].includes(letter)){
+      const results = LETTERS
+        .filter(letter => grid[0].includes(letter))
+        .map(letter => {
           const [start, end] = [[0, grid[0].indexOf(letter)], [0, lastIndexOfRun(letter, grid[0], grid[0].indexOf(letter))]];
           longest = Math.max(longest, end[1]-start[1]);
-          results.push({letter, start, end});
-        }
-      }
+          return {letter, start, end};
+        });
+
       return results.filter(result => result.end[1]-result.start[1] >= longest);
     }
   }
@@ -23,5 +23,5 @@ function lastIndexOfRun(letter, string, firstIndexOf){
       return i -1 ;
     }
   }
-  return string.length - 1;
+  return --string.length;
 }
